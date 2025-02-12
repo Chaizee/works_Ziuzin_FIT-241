b6ed04ee6cb39ec861a6a2fb9ddabebbed995f5c
Chaizee <zuzindima26@gmail.com>
2025-02-12 10:56:26 +0600

    Update Program.cs

--- 2 семестр/Лабы ДМ/Program.cs
+++ 2 семестр/Лабы ДМ/Program.cs
@@ -1,66 +1,132 @@
-﻿class Program
+//Первый алгоритм
+
+class Program
 {
-    
+
     public static void Main(string[] args)
     {
         Console.WriteLine("Введите количество вершин");
         int n = int.Parse(Console.ReadLine());
         int[,] graf = new int[n, n];
         Console.WriteLine("Введите матрицу смежности");
-        for (int i = 0; i <n; i++)
+        for (int i = 0; i < n; i++)
         {
-            for (int j = 0; j <n; j++)
+            for (int j = 0; j < n; j++)
             {
-                graf[i,j] = int.Parse(Console.ReadLine());
+                graf[i, j] = int.Parse(Console.ReadLine());
             }
         }
-        //int n = 5;
-        //int[,] graf = new int[5, 5] { { 0, 1, 0, 0,0 }, { 1, 0, 1, 0,0 }, { 0, 1, 0, 0,0 }, { 0, 0, 0, 0,1 },{ 0,0,0,1,0} };
         List<int> list1 = new List<int>();
         for (int i = 1; i <= n; i++)
         {
             list1.Add(i);
         }
 
-        List<int> listt = new List<int> { };
-        listt.Add(1);
-        list1.Remove(1);
-        int c = 0;
+
+        int count = 0;
         while (list1.Count != 0)
         {
-            c++;
-            for (int i = c; i <= listt.Count; i++)
+            List<int> listt = new List<int> { };
+            listt.Add(list1[0]);
+            list1.Remove(list1[0]);
+            int c = 0;
+            while (list1.Count != 0)
             {
-                int k = listt[i - 1];
-
-                for (int j = 0; j < n; j++)
+                c++;
+                for (int i = c; i <= listt.Count; i++)
                 {
-                    if (graf[k - 1, j] == 1)
+                    int k = listt[i - 1];
+
+                    for (int j = 0; j < n; j++)
                     {
-                        listt.Add(j + 1);
-                        list1.Remove(j + 1);
+                        if (graf[k - 1, j] == 1)
+                        {
+                            listt.Add(j + 1);
+                            list1.Remove(j + 1);
+                        }
                     }
+                    break;
                 }
-                break;
+                HashSet<int> hah = new HashSet<int>(listt);
+                listt = hah.ToList();
+                if (c == n + 1)
+                {
+                    break;
+                }
             }
-            HashSet<int> hah = new HashSet<int>(listt);
-            listt = hah.ToList();
-            if (c == n + 1)
+            count++;
+        }
+        Console.WriteLine($"Граф имеет {count} компонент связанностей");
+
+    }
+}
+
+//Второй алгоритм
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите количество вершин");
+        int n = int.Parse(Console.ReadLine());
+        int[,] graf = new int[n, n];
+        Console.WriteLine("Введите матрицу смежности");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
             {
-                break;
+                graf[i, j] = int.Parse(Console.ReadLine());
             }
         }
-        for (int i = 0; i < listt.Count; i++)
+
+        List<int> list1 = new List<int>();
+        for (int i = 1; i <= n; i++)
         {
-            Console.WriteLine(listt[i]);
+            list1.Add(i);
         }
-        if (list1.Count == 0)
+
+        List<int> listt = new List<int>();
+        listt.Add(1);
+        int c = 0;
+        while (listt.Count != n)
         {
-            Console.WriteLine("Граф связанный");
+            c++;
+            List<int> temp = new List<int>();
+            for (int i = 0; i <= c - 1; i++)
+            {
+
+                if (graf[c, i] == 1)
+                {
+                    temp.Add(listt[i]);
+
+                }
+            }
+
+            if (listt.Count != c + 1)
+            {
+                temp.Add(listt[c - 1] + 1);
+
+            }
+            listt.Add(Enumerable.Min(temp));
+
         }
-        else
+
+        for (int i = 1; i < listt.Count; i++)
         {
-            Console.WriteLine("Граф не связанный");
+            for (int j = 0; j < i; j++)
+            {
+                if (listt[i] < listt[j])
+                {
+
+                    if (graf[i, j] == 1)
+                    {
+                        listt[j] = listt[i];
+                    }
+
+                }
+            }
         }
+        
+        Console.WriteLine($"Граф имеет {Enumerable.Max(listt)} компонент связанностей");
     }
 }
