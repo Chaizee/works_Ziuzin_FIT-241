eeadf64f0f859babc7ca29f791f4a2c90ffed03e
Chaizee <zuzindima26@gmail.com>
2025-02-21 12:45:03 +0600

    Add files via upload

---
+++ 2 семестр/Лабы АиП/lab-21.02/Program.cs
@@ -0,0 +1,57 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var stack = new Stack<char>();
+        string str = Console.ReadLine();
+
+        foreach (var c in str)
+        {
+            switch (c)
+            {
+                case '(':
+                    stack.Push(c);
+                    break;
+                case '{':
+                    stack.Push(c);
+                    break;
+                case '[':
+                    stack.Push(c);
+                    break;
+                case ')':
+                    if (stack.Count == 0 || stack.Peek() != '(')
+                    {
+                        Console.WriteLine("Неправильно задано выражение");
+                        return;
+                    }
+                    stack.Pop();
+                    break;
+                case '}':
+                    if (stack.Count == 0 || stack.Peek() != '{')
+                    {
+                        Console.WriteLine("Неправильно задано выражение");
+                        return;
+                    }
+                    stack.Pop();
+                    break;
+                case ']':
+                    if (stack.Count == 0 || stack.Peek() != '[')
+                    {
+                        Console.WriteLine("Неправильно задано выражение");
+                        return;
+                    }
+                    stack.Pop();
+                    break;
+            }
+        }
+        if (stack.Count == 0)
+        {
+            Console.WriteLine("Выражение задано верно");
+        }
+
+        else
+        {
+            Console.WriteLine("Неправильно задано выражение");
+        }
+    }
+}
