44a9a11a2a2fa20cbd8df1c59b2a467a7327b0cb
Chaizee <zuzindima26@gmail.com>
2025-03-25 00:00:52 +0600

    New Lab

--- 2 семестр/Лабы АиП/lab-28.02/Program.cs
+++ 2 семестр/Лабы АиП/lab-28.02/Program.cs
@@ -1,4 +1,5 @@
-﻿class Program
+﻿// 1 Задача. Обратная польская запись(постфиксная)
+class Program
 {
     public static void Main(string[] args)
     {
@@ -49,6 +50,43 @@
         else
         {
             Console.WriteLine("Выражение задано неверно");
+        }
+    }
+}
+
+// 2 Задача. Словарь
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите числовую последовательность (чтобы перестать введите '.')");
+        List<int> list = new List<int>();
+        string StrInput = Console.ReadLine();
+
+        while (StrInput != ".")
+        {
+            list.Add(int.Parse(StrInput));
+            StrInput = Console.ReadLine();
+        }
+
+        Dictionary<int, int> Counts = new Dictionary<int, int>();
+
+        foreach (var i in list)
+        {
+            if (!Counts.ContainsKey(i))
+            {
+                Counts[i] = 1;
+            }
+            else
+            {
+                Counts[i]++;
+            }
+        }
+
+        foreach (var (key, val) in Counts)
+        {
+            Console.WriteLine($"Значение: {key}, Количество: {val}");
         }
     }
 }
