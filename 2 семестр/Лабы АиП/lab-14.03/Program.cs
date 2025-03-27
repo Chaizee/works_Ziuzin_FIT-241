b0ae8460de4054d632f1813e5dba118528a608f0
Chaizee <zuzindima26@gmail.com>
2025-03-27 23:54:57 +0600

    New Lab

--- 2 семестр/Лабы АиП/lab-14.03/Program.cs
+++ 2 семестр/Лабы АиП/lab-14.03/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace Delegates
+// Задача 1
+namespace Delegates
 {
 
     public class Calculate
@@ -79,3 +80,75 @@
     }
 }
 
+// Задача 2
+
+class Car
+{
+    public string Year { get; set; }
+    public string Model { get; set; }
+    public string Color { get; set; } 
+    public bool IsWashed { get; set; } 
+
+    public Car (string year, string model, string color, bool isWashed)
+    {
+        Year = year;
+        Model = model;
+        Color = color;
+        IsWashed = isWashed;
+    }
+}
+
+class Garage
+{
+    public List<Car> Cars;
+}
+
+class Washing
+{
+    public static void washCar(Car car)
+    {
+        if (car.IsWashed == true)
+        {
+            Console.WriteLine($"Машина: {car.Model} чистая");
+        }
+        else
+        {
+            car.IsWashed = true;
+            Console.WriteLine($"Машина: {car.Model} помыта");
+        }
+    }
+}
+
+delegate void CarWashing(Car car);
+class Program
+{
+    static void Main(string[] args)
+    {
+        CarWashing carWashing = new CarWashing(Washing.washCar);
+
+        Garage garage = new Garage();
+        garage.Cars = new List<Car>();
+
+        Console.Write("Введите количество машин: ");
+        int CarsCount = Convert.ToInt32(Console.ReadLine());
+        
+        for(int i=0; i < CarsCount; i++)
+        {
+            Console.Write("Введите модель машины: ");
+            string Model = Console.ReadLine();
+            Console.Write("Введите год выпуска: ");
+            string Year = Console.ReadLine();
+            Console.Write("Введите цвет машины: ");
+            string Color = Console.ReadLine();
+            Console.WriteLine("Машина помыта или нет? (Введите 1 - если да, 0 - если нет)");
+            bool CarIsWashed = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine()));
+
+            garage.Cars.Add(new Car(Year, Model, Color, CarIsWashed));
+        }
+
+        for (int i =0; i <CarsCount; i++)
+        {
+            carWashing(garage.Cars[i]);
+        }
+    }
+}
