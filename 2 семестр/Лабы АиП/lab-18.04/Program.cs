7953f29d699944b294bc70b434b7474c0d826982
Chaizee <zuzindima26@gmail.com>
2025-04-29 17:13:40 +0300

    feat: new lab

---
+++ 2 семестр/Лабы АиП/lab-18.04/Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> list = File.ReadAllLines("input.txt").ToList();
+
+        using (StreamWriter writer = new StreamWriter("output.txt"))
+        {
+
+
+            foreach (string line in list)
+            {
+                string tempString = null;
+
+                for (int i = 0; i < line.Length; i++)
+                {
+
+                    if (char.IsDigit(line[i]))
+                    {
+                        tempString += line[i];
+                    }
+
+                    if (!char.IsDigit(line[i]) && tempString != null)
+                    {
+                        if ((Convert.ToInt32(tempString)) % 2 == 0)
+                        {
+                            writer.WriteLine(line);
+                            break;
+                        }
+                        tempString = null;
+                    }
+                }
+            }
+        }
+    }
+}
