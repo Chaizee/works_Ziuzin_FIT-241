2c20ce5cea1416e412cc24bbe986c932099c7f98
Chaizee <zuzindima26@gmail.com>
2025-05-16 12:22:10 +0600

    feat: new  lab

---
+++ 2 семестр/Лабы АиП/lab-16.05/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    unsafe static void Main()
+    {
+        int[] mus = new int[5];
+
+        Console.WriteLine("Введите элементы массива");
+        for (int i = 0; i < 5; i++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            mus[i] = n;
+        }
+
+        fixed (int* ptr = mus)
+        {
+            int* p = ptr;
+            int Count = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (*p % 2 == 0)
+                {
+                    ++Count;
+                }
+                p++;
+            }
+            Console.WriteLine(Count);
+        }
+        
+    }
+}
